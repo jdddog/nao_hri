9d59c502b68df7a06433675648db2c489ae85bdc
jdddog <jamie.diprose@gmail.com>
2014-09-18 00:59:06 +1200

    Changes to enable use of blender target controllers

--- src/nao_hri/__init__.py
+++ src/nao_hri/__init__.py
@@ -1,5 +1,6 @@
 from .keyframe_animation import *
 from .nao import *
+from .nao_joint import *
 from .nao_driver_naoqi import *
 from .nao_gesture_action_srv import *
 from .nao_tts_action_srv import *
